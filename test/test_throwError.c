537b1e909964e4632f904da0ecd54069d06cc0bc
jwchong93 <jwchong93@gmail.com>
2014-03-27 23:09:28 +0800

    Added the testing code for getToken to throw an error while meet undefined identifier.

--- test/test_throwError.c
+++ test/test_throwError.c
@@ -31,6 +31,7 @@ void test_getToken_will_update_the_error_message_when_it_meet_unknown_operator()
 		TEST_ASSERT_EQUAL_STRING("Unknown operator ! ",errorMessage.message);
 		
 	}
+	free(testTokenizer);
 	
 }
 
@@ -53,7 +54,7 @@ void test_getToken_will_update_the_error_message_when_it_meet_invalid_identifier
 		TEST_ASSERT_EQUAL_STRING("Invalid identifier ! ",errorMessage.message);
 		
 	}
-	
+	free(testTokenizer);
 }
 
 void test_getToken_will_update_the_error_message_when_it_meet_unknown_operator_in_different_location()
@@ -78,5 +79,80 @@ void test_getToken_will_update_the_error_message_when_it_meet_unknown_operator_i
 		TEST_ASSERT_EQUAL_STRING("Unknown operator ! ",errorMessage.message);
 		
 	}
-	
+	free(testTokenizer);
 }
+
+void test_getToken_throw_an_error_while_the_identifier_is_not_defined()
+{
+	String *testTokenizer = stringCreate ("pptr+12");
+	Token *testToken;
+	Error exception;
+	char *name = "pptr";
+	getElement_ExpectAndReturn(DefineList, name,NULL);
+	
+	Try
+	{
+		testToken = getToken (testTokenizer);
+	}
+	Catch(exception)
+	{
+		TEST_ASSERT_EQUAL(UNDEFINED_IDENTIFIER,exception);
+		
+		// Check the error message been updated or not . 
+		TEST_ASSERT_EQUAL_STRING("pptr+12",errorMessage.rawString);
+		TEST_ASSERT_EQUAL(0,errorMessage.position);
+		TEST_ASSERT_EQUAL_STRING("Undefined Identifier ! ",errorMessage.message);
+		
+	}
+	free(testTokenizer);
+}
+
+void test_getToken_throw_an_error_while_the_undefined_identifier_is_behind_the_expression()
+{
+	String *testTokenizer = stringCreate ("12+4*2-45/pptr");
+	Token *testToken;
+	Error exception;
+	char *name = "pptr";
+	getElement_ExpectAndReturn(DefineList, name,NULL);
+	free(getToken(testTokenizer));//12
+	free(getToken(testTokenizer));//+
+	free(getToken(testTokenizer));//4
+	free(getToken(testTokenizer));//*
+	free(getToken(testTokenizer));//2
+	free(getToken(testTokenizer));//-
+	free(getToken(testTokenizer));//45
+	free(getToken(testTokenizer));// /
+	
+	
+	Try
+	{
+		testToken = getToken (testTokenizer);
+	}
+	Catch(exception)
+	{
+		TEST_ASSERT_EQUAL(UNDEFINED_IDENTIFIER,exception);
+		
+		// Check the error message been updated or not . 
+		TEST_ASSERT_EQUAL_STRING("12+4*2-45/pptr",errorMessage.rawString);
+		TEST_ASSERT_EQUAL(10,errorMessage.position);
+		TEST_ASSERT_EQUAL_STRING("Undefined Identifier ! ",errorMessage.message);
+		
+	}
+	free(testTokenizer);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
